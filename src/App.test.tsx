43947f838c347b72018f94bf8a193fbe64bcb35d
Keshmiry Alireza <ark13da@gmail.com>
2021-09-09 10:46:19 +0300

    workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import { render} from '@testing-library/react';
+import { render,screen,fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-  test('render App components title', () => {
+describe('Test App component rendering', () => {
+  
+  it('renders App components title', () => {
     const { getByText } = render(
       <Provider store={store}>
         <App />
@@ -12,3 +14,27 @@ import App from './App';
     expect(getByText(/Lord/i)).toBeInTheDocument();
   });
 
+});
+
+/*
+describe('UserInput data validation', () => {
+  
+  it('throws an error for non numerical inputs', async () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+ jest.setTimeout(30000);
+    const inputElement: any = screen.getByPlaceholderText(/enter the deck id/i);
+    await fireEvent.change(inputElement, { target: { value: "apples" } });
+    await fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter", keyCode:13,
+      charCode: 13
+    });
+   
+    const titleElement = await screen.findByText(/try/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+});
+*/
